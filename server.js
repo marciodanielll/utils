ae5fd6dd070243fd1a046d29272b6f8a3f5df306
M.D <marcio.daniel@msn.com>
2025-08-29 18:06:04 -0300

    fix to alphine dist

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Endpoint para pegar estatísticas do sistema
 app.get("/api/stats", (req, res) => {
-  // Comando para pegar uso de CPU/memória e top 5 processos por uso de CPU/memória
   const cmd = `top -b -n1 | head -n 12 && ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 6`;
   exec(cmd, (err, stdout, stderr) => {
     if (err)
